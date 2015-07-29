b8e635b8bdfe8a9d7b33c47d5172bd9225eb60a6
afriant1 <amandafriant@gmail.com>
2015-07-29 09:44:22 -0500

    Completes Part 3

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,34 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @id = params["id"]
+    Photo.find_by({ :id => @id })
+    render("show.html.erb")
+  end
+
+  def new_form
+    render("new_form.html.erb")
+  end
+
+  def edit
+    @id = params["id"]
+    Photo.find_by({ :id => @id })
+    render("edit.html.erb")
+  end
+
+#  def update
+#    @id = params["id"]
+#    @photo = Photo.find_by({ :id => @id })
+#    @photo.update!(caption_params)
+#    redirect_to("/#{{:id}}.html")
+#    render("show.html.erb")
+#  end
+
+  def destroy
+    @id = params["id"]
+    Photo.find_by({ :id => @id }).destroy
+    redirect_to url: "http://localhost3000/photos"
+  end
 
 end
